6f4b21d16b527b9c91b82ee267210d005082af71
Rasmus Lerchedahl Petersen <rusmus@eecs.qmul.ac.uk>
2013-05-15 10:55:00 +0100

    Use exposed global prefix

--- src/topl/toplNames.ml
+++ src/topl/toplNames.ml
@@ -6,4 +6,4 @@ let s = Psyntax.mkString
 let call_event p = s& sprintf "call_$$_%s" p
 let return_event p = s& sprintf "return_$$_%s" p
 
-let global n = "$g_" ^ n
+let global n = sprintf "%s_%s" CoreOps.global_prefix n
